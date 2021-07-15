c962b7c0d7683035891bee423c8e71752bceda47
riad <skmdriad@gmail.com>
2021-07-15 23:42:18 +0800

    finishing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseli
 import { PhotoCamera } from '@material-ui/icons';
 import useStyles from './styles';
 
-
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const App = () =>{
     const classes = useStyles();
     return(
@@ -46,28 +46,40 @@ const App = () =>{
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing = {4}>
-                        <Grid item>
-                            <Card className={classes.card}>
-                                <CardMedia
-                                    className={classes.cardMedia}
-                                    image = "https://source.unsplash.com/random"
-                                    title = "Image title"
-                                />
-                                <CardContent className={classes.cardContent}>
-                                    <Typography gutterBottom variant="h5">
-                                        Heading 
-                                    </Typography>
-                                    <Typography>
-                                        This is a media card. You can use this section to describe the content.
-                                    </Typography>
-                                </CardContent>
+                        {
+                            cards.map((card)=>(
+                                <Grid item key={card} xs={12} sm={6} md={4}>
+                                    <Card className={classes.card}>
+                                        <CardMedia
+                                            className={classes.cardMedia}
+                                            image = "https://source.unsplash.com/random"
+                                            title = "Image title"
+                                        />
+                                        <CardContent className={classes.cardContent}>
+                                            <Typography gutterBottom variant="h5">
+                                                Heading 
+                                            </Typography>
+                                            <Typography>
+                                                This is a media card. You can use this section to describe the content.
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small" color="primary">View</Button>
+                                            <Button size="small" color="primary">Edit</Button>
+                                        </CardActions>
 
-                            </Card>
-                        </Grid>
+                                    </Card>
+                                </Grid>
+                            ))
+                        }                        
 
                     </Grid>
                 </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom> Copyright </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary"> 2021 Riad</Typography>
+            </footer>
         </>
     );
 }
